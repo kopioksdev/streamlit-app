70eb4f88867a5de8c228befa91364dc526264f2a
kopioksdev <kopioksdev@gmail.com>
2024-12-13 06:16:13 +0000

    changed header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new Streamlit app test")
+st.write("Hello World 1 2")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
